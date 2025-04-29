7e289ecc89cb8c990d2060b5bb09c8070251a349
jacobstarheim <grovinstar@gmail.com>
2025-04-29 05:19:37 +0200

    mvp setup

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,103 +1,145 @@
-import Image from "next/image";
+'use client';
+import { useState, useEffect } from 'react';
+import {
+    GoogleAuthProvider,
+    signInWithPopup,
+    signOut,
+    onAuthStateChanged,
+    User,
+} from 'firebase/auth';
+import {auth} from '../lib/firebase/config';
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+export default function HomePage() {
+    const [user, setUser] = useState<User | null>(null);
+    const [loadingAuth, setLoadingAuth] = useState(true);
+    const [showTips, setShowTips] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+    const handleSignIn = async () => {
+        const provider = new GoogleAuthProvider();
+
+        try {
+            await signInWithPopup(auth, provider);
+            console.log('Sign in succesfull.')
+            setShowTips(false);
+        } catch (error) {
+            console.error(`Error signing in with Google ${error}`);
+            alert('Innlogging med Google mislyktes. Vennligst prøv igjen.')
+        }
+    };
+
+  const handleSignOut = async () => {
+        try {
+            await signOut(auth);
+            console.log('Sign out successful');
+            setShowTips(false);
+        } catch (error) {
+            console.error('Error signing out:', error);
+            alert('Utlogging med Google feilet. Vennligst prøv igjen');
+        }
+    };
+
+    useEffect( () => {
+
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoadingAuth(false);
+            if (!currentUser) {
+                setShowTips(false)
+            }
+            console.log(`Auth state changed for user ${currentUser?.email}`)
+        });
+        
+        return () => unsubscribe();
+    }, 
+    [] ); 
+
+    if (loadingAuth) {
+        return <p className="p-4">Laster...</p>;
+        // TODO: Lag en bedre loading indikator
+    }
+
+    return (
+    <main className="p-4">
+        <h1 className="text-xl font-bold mb-4">Karbonkalkulator</h1>
+
+        {user ? (
+            <>
+            {/*bruker innlogget*/}
+
+                <div>
+                    <p className="mb-2">Velkommen.</p>
+                    <button
+                        onClick={handleSignOut}
+                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                    >
+                        Logg ut
+                    </button>
+                </div>
+
+
+                <div className="mb-6 p-4 border border-gray-300 rounded">
+                    <h2 className="text-lg font-semibold mb-2">Loggfør Aktivitet</h2>
+                    {/* ActivityForm her */}
+                    <p className="text-gray-500 italic">(Plassholder for ActivityForm)</p>
+                    {/* TODO: Vis kvittering her etter logging */}
+                </div>
+
+                {/* TODO: legge in activityform og activitylist */}
+                <div className="mt-6 p-4 border border-dashed border-gray-400">
+                    <p> Plassholder for Aktivitetslogging og Historikk - vises kun når innlogget</p>
+                </div>
+
+
+                <div className="mb-6 p-4 border border-gray-300 rounded">
+                    <h2 className="text-lg font-semibold mb-2">Din Historikk</h2>
+                    {/* ActivityList her */}
+                    <p className="text-gray-500 italic">(Plassholder for ActivityList)</p>
+                    {/* TODO: Legg til totalberegning her */}
+                </div>
+
+
+                <div className="mb-6 p-4 border border-gray-300 rounded">
+                    <h2 className="text-lg font-semibold mb-2">Tips for Lavere Utslipp</h2>
+                    {!showTips && ( // Vis knappen kun hvis tips er skjult
+                    <button
+                        onClick={() => setShowTips(true)}
+                        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                    >
+                        Vis Tips
+                    </button>
+                    )}
+
+
+                    {showTips && (
+                        <div>
+                            {/* TODO: TipsComponent her */}
+                            <p className="text-gray-500 italic">(Plassholder for TipsComponent)</p>
+                            {/* TODO: Mulighet for å skjule tips igjen */}
+                        </div>
+                    )}
+
+                </div>
+            </>
+
+        ) : (
+            // Bruker utlogget
+
+            <div>
+                <p className="mb-2">Vennligst logg inn for å bruke kalkulatoren.</p>
+                <button
+                    onClick={handleSignIn}
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                    Logg inn med Google
+                </button>
+            </div>
+        )}
+
+    </main>
   );
 }
+
+
+
+
